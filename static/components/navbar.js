e7094d36076582043d2d3c442fa31567b47c11e4
codx_venu <vasulallu09@gmail.com>
2025-09-08 01:31:36 +0530

    ok

--- static/components/navbar.js
+++ static/components/navbar.js
@@ -56,6 +56,47 @@ export default {
             @mouseover="hoverButton($event)" 
             @mouseleave="resetButton($event)">Search</button>
         </form>
+        <!-- No Results Modal -->
+        <div v-if="searchPerformed && !results.length" class="modal fade show d-block" tabindex="-1" role="dialog">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">No Results Found</h5>
+                  style="background-color: transparent; border: none;">&times;</button>
+              </div>
+              <div class="modal-body">
+                <p>No results match your search query. Please try again.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" @click="searchPerformed = false;" 
+                  style="border-radius: 5px;">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Results Modal -->
+        <div v-if="results.length" class="modal fade show d-block" tabindex="-1" role="dialog">
+          <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Search Results</h5> 
+                  style="background-color: transparent; border: none;">&times;</button>
+              </div>
+              <div class="modal-body">
+                <ul>
+                  <li v-for="result in results" :key="result.id">
+                    {{ result.name || result.title || result.username || result.text || result.email }}
+                  </li>
+                </ul>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" @click="results = []; searchPerformed = false;" 
+                  style="border-radius: 5px;">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <!-- User Authentication Links -->
         <ul class="navbar-nav align-items-center">
