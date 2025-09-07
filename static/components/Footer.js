19231fcd470e1ad731fc5f293a6031a0dc0b8578
MADHAV SHARMA <madhavsharma8194@gmail.com>
2025-09-07 21:12:09 +0530

    Update Footer.js

--- static/components/Footer.js
+++ static/components/Footer.js
@@ -1,11 +1,8 @@
 
 export default {
     template: `
-    <div class="row bg-secondary text-white text-center py-4">
-        <div class="col">
-            <div class="text-center mt-2">
-                <p> QUIZ MASTER  | &copy; copyright 2025 | created  by 23F3004142 </p>
-            </div>
-        </div>
-    </div>`
+    <div style="background-color: whitesmoke; color: #242424; text-align: center; padding: 16px; box-shadow: 0 2px 6px rgba(0,0,0,0.2);">
+        <p style="margin: 0; font-size: 14px; color: #242424;">QUIZ MASTER  | &copy; copyright 2025 | created by 23F3004142</p>
+    </div>
+    `
 }
